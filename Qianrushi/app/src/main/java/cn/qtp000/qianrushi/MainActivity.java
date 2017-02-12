4848abbe3679cd114c3d48cc31f525e9820f1c2f
祁天培 <qtp1300@163.com>
2017-02-12 16:14:29 +0800

    新建不同Class

--- Qianrushi/app/src/main/java/cn/qtp000/qianrushi/MainActivity.java
+++ Qianrushi/app/src/main/java/cn/qtp000/qianrushi/MainActivity.java
@@ -2,6 +2,13 @@ package cn.qtp000.qianrushi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static cn.qtp000.qianrushi.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,8 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        findViewById(R.id.textView2);
-
+        ImageButton upbtn = (ImageButton) findViewById(R.id.upButton);
+        ImageButton leftbtn = (ImageButton) findViewById(R.id.leftButton);
+        ImageButton rightbtn = (ImageButton) findViewById(R.id.RIGHTButton);
+        ImageButton downbtn = (ImageButton) findViewById(R.id.downButton);
+        upbtn.setOnClickListener(new anjian());
+        leftbtn.setOnClickListener(new anjian());
+        rightbtn.setOnClickListener(new anjian());
+        downbtn.setOnClickListener(new anjian());
     }
+    class anjian implements android.view.View.OnClickListener{
+        @Override
+        public void onClick(android.view.View view) {
+            switch (view.getId())
+            {
+                case R.id.upButton:
+                    Toast.makeText(MainActivity.this,"上键",Toast.LENGTH_SHORT).show();
+                    System.out.println("阿萨德");
+                    break;
+                case R.id.leftButton:
+                    Toast.makeText(MainActivity.this,"左键",Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.RIGHTButton:
+                    Toast.makeText(MainActivity.this,"右键",Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.downButton:
+                    Toast.makeText(MainActivity.this,"下键",Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+    }
+
 }
---
+++ Qianrushi/app/src/main/java/function/function.java
@@ -0,0 +1,8 @@
+package function;
+
+/**
+ * Created by 祁天培 on 2017/2/12.
+ */
+
+public class function {
+}
